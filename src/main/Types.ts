87e533b55f59e9e9a272294ce3a69a2fe3f86d75
Matthew Adams <matthew@matthewadams.me>
2019-08-01 15:10:04 -0500

    add (broken) typedoc

--- src/main/Types.ts
+++ src/main/Types.ts
@@ -1,34 +1,102 @@
 /**
  * Some convenient types.
  */
+
+/**
+ * `string | undefined`
+ */
 export type stringu = string | undefined;
+/**
+ * `string | null`
+ */
 export type stringn = string | null;
+/**
+ * `string | undefined | null`
+ */
 export type stringun = stringu | stringn;
 
+/**
+ * `number | undefined`
+ */
 export type numberu = number | undefined;
+/**
+ * `number | null`
+ */
 export type numbern = number | null;
+/**
+ * `number | undefined | null`
+ */
 export type numberun = numberu | numbern;
 
+/**
+ * `boolean | undefined`
+ */
 export type booleanu = boolean | undefined;
+/**
+ * `boolean | null`
+ */
 export type booleann = boolean | null;
+/**
+ * `boolean | undefined | null`
+ */
 export type booleanun = booleanu | booleann;
 
-export type arrayu<T> = T[] | undefined;
-export type arrayn<T> = T[] | null;
-export type arrayun<T> = arrayu<T> | arrayn<T>;
-
+/**
+ * `object | undefined`
+ */
 export type objectu = object | undefined;
+/**
+ * `object | null`
+ */
 export type objectn = object | null;
+/**
+ * `object | undefined | null`
+ */
 export type objectun = objectu | objectn;
 
+/**
+ * `any | undefined`
+ */
 export type anyu = any | undefined;
+/**
+ * `any | null`
+ */
 export type anyn = any | null;
+/**
+ * `any | undefined | null`
+ */
 export type anyun = anyu | anyn;
 
+/**
+ * `array<T> | undefined`
+ */
+export type arrayu<T> = T[] | undefined;
+/**
+ * `array<T> | null`
+ */
+export type arrayn<T> = T[] | null;
+/**
+ * `array<T> | undefined | null`
+ */
+export type arrayun<T> = arrayu<T> | arrayn<T>;
+
+/**
+ * `undefined | null`
+ */
 export type undefinedn = undefined | null;
+
+/**
+ * Alias for [`undefinedn`].
+ */
 export type nullu = undefinedn;
 
+/**
+ * A constructor.
+ */
 export type ctor<T = {}> = new (...args: any[]) => T;
 
+/**
+ * The empty class.
+ */
 export class Empty {
 }
